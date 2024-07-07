1a84330365b6415ef701df3019badef34f9e0fae
yeni_bababatunde <155441386+omoyeni-babatunde-iOS-fcmb@users.noreply.github.com>
2024-07-07 14:32:00 +0100

    Update AppConstants.swift

--- MovieDBList/MovieDBList/Utils/AppConstants.swift
+++ MovieDBList/MovieDBList/Utils/AppConstants.swift
@@ -20,4 +20,17 @@ enum AppConstants {
             "\(BASE_URL)\(type)/\(id)/images?"
         }
     }
+    
+    
+}
+
+var infoPlistProperty: [String: Any]? {
+    if  let path = Bundle.main.path(forResource: "Info", ofType: "plist"), let xml = FileManager.default.contents(atPath: path) {
+        return (try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil)) as? [String: Any]
+    }
+    return nil
+}
+
+func infoPlistString(key: String) -> String {
+    infoPlistProperty![key] as! String
 }
